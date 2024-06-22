3ababb54321795602aa02e6eee268c86e2dd267e
tkm-mkzk <tkm.mkzk.0604@gmail.com>
2024-06-22 16:20:49 +0900

    テーブルとダミーデータ作成

---
+++ src/database/factories/ContactFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ContactFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'first_name' => $this->faker->firstName,
+            'last_name' => $this->faker->lastName,
+            'gender' => $this->faker->numberBetween(0, 2),
+            'email' => $this->faker->safeEmail,
+            'tel' => $this->faker->phoneNumber,
+            'address' => $this->faker->address,
+            'building' => $this->faker->secondaryAddress,
+            'category_id' => $this->faker->numberBetween(1, 5),
+            'detail' => $this->faker->text(200),
+        ];
+    }
+}
